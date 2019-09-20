fe316031847e8ff8ee8d09638005126ec4506499
Valentin Salome <vasalome@z2r5p4.le-101.fr>
2019-09-20 15:41:51 +0200

    try_ft_file_exist_or_dir_but_nope

--- srcs_wolf/main_wolf.c
+++ srcs_wolf/main_wolf.c
@@ -6,33 +6,12 @@
 /*   By: vasalome <vasalome@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/08 17:53:57 by vasalome     #+#   ##    ##    #+#       */
-/*   Updated: 2019/09/19 19:11:52 by vasalome    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/09/20 15:38:14 by vasalome    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes_wolf/wolf.h"
-
-int		check_map(t_info *info)
-{
-	if (!(ft_strcmp(info->map.name, "maps/mindfuck.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/big_cube.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/lnieto_1.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/lnieto_2.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/vasalome_1.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/vasalome_2.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/test_texture.map")))
-		return (0);
-	if (!(ft_strcmp(info->map.name, "maps/test_tp.map")))
-		return (0);
-	return (64);
-}
 
 int		main(int argc, char **argv)
 {
